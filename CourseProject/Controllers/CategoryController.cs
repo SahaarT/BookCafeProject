f5d675cfb0496d148bed55ad0f1420b88cf24d0d
SahaarT <74072766+SahaarT@users.noreply.github.com>
2023-06-17 13:00:35 +0430

    Add project files.

---
+++ CourseProject/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using CourseProject.Data;
+using CourseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == "")
+            {
+                ModelState.AddModelError("Name field", "The Name Field Cannot Be Empty!!!!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Get
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category category = _db.Categories.Find(Id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                return View(category);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Updated Successfully";
+            return RedirectToAction("Index");
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View();
+            }
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(Category category)
+        {            
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CourseProject/Migrations/20230425081018_changeCreatedDateTime-In-Table-Category-To-Timestamp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseProject.Migrations
+{
+    public partial class changeCreatedDateTimeInTableCategoryToTimestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
